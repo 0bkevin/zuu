1639b52e1a01fc083983af1bd355f71fd7c76360
Kevin Bravo <bravokevinto@gmail.com>
2024-11-14 21:04:42 -0400

    fix(ts/react/free2z): handle case when converse comment doesn't exist

--- ts/react/free2z/src/components/Converse.tsx
+++ ts/react/free2z/src/components/Converse.tsx
@@ -1,27 +1,43 @@
 import { useParams } from "react-router-dom";
 import { CommentData } from "./DisplayThreadedComments";
-import { useEffect, useState } from "react";
+import { lazy, useEffect, useState } from "react";
 import axios from "axios";
 import { Grid } from "@mui/material";
 import DisplayThreadedComment from "./DisplayThreadedComment";
+import LoadingBackdrop from "./LoadingBackdrop";
 
+const Global404 = lazy(() => import('../Global404'))
 
 export default function Converse() {
-    const params = useParams()
-    const parent = params.commentUUID
+    const params = useParams();
+    const parent = params.commentUUID;
+    const [loading, setLoading] = useState(true);
 
-    const [comment, setComment] = useState({} as CommentData)
+    const [comment, setComment] = useState<CommentData | null>(null);
 
     useEffect(() => {
         axios.get(`/api/comments/${parent}/`).then(res => {
-            // console.log("COMMENT", res)
             setComment(res.data)
+        }).catch(() => {
+            // prevent error when comment is not found
+        }).finally(() => {
+            setLoading(false);
         })
     }, [parent])
 
-    if (!comment.uuid) {
-        return null
-    }
+    if (loading) return <LoadingBackdrop />
+    if (!comment || !comment.uuid) return(
+        <div style={{
+            // use the entire screen
+            position: "absolute",
+            top: 0,
+            bottom: 0,
+            left: 0,
+            right: 0,
+        }}>
+             <Global404 />
+        </div>
+    )
 
     return (
         <Grid
